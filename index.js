03c09066e8b17885e36c8cea9f60f597c7fd3be1
Thiago <thiago.ralves02@gmail.com>
2022-09-01 21:35:28 -0300

    feat: numero inicial sendo exibido

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var erro = 0
 
 window.onload = function () {
     geraNumeroAleatorio()
+    exibeNumeroInicial()
 }
 
 async function geraNumeroAleatorio() {
@@ -21,4 +22,22 @@ function validaPalpite(){
 
     document.querySelector("#palpite").value = ''
     return numeroAleatorio != 0 && palpite > numeroAleatorio ? console.log("É menor") : console.log("É maior")
+}
+
+function exibeNumeroInicial(){
+    var numero
+    erro == 0 ? numero = numZero() : numero =  numErro()
+
+    for(i=0;i<numero.length;i++){
+        var linha = document.getElementById(numero[i]);
+        linha.style.fill = "#262A34";
+    }
+}
+
+function numZero(){
+    return ['a', 'b', 'c', 'd', 'e', 'f','e']
+}
+
+function numErro(){
+
 }
