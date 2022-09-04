ae88b59197483dc43382aaa919759997ff89df9c
Thiago <thiago.ralves02@gmail.com>
2022-09-04 18:56:11 -0300

    feat: funçao para permitir apenas numeros no input

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Variveis globais
 var numeroAleatorio = 0
 var erro = 0
 var palpite = 0
@@ -30,7 +31,6 @@ function validaNumeroAleatorio(req) {
 function validaPalpite(){
     palpite = document.querySelector("#palpite").value
     document.querySelector("#palpite").value = ''
-
     exibeNumero(palpite)
     funcionalidadesBotoes()
     exibeInfoPalpite()
@@ -38,15 +38,22 @@ function validaPalpite(){
 
 //Função para as funcionalidades dos botões
 //Alterar visibilidade e disabled dos botões conforme palpite ou erro
+//Habilita o botão de enviar quando o input estiver preenchido
 function funcionalidadesBotoes() {
+    var inputPalpite = document.getElementById(`palpite`)
+
     if(erro != 0 || palpite == numeroAleatorio){
         document.getElementById(`novaPartida`).style.display = 'flex'
         document.getElementById(`palpite`).disabled = true;
         document.getElementById(`enviar`).disabled = true;
-    } else {
+    } else if( inputPalpite.value == ''){
         document.getElementById(`novaPartida`).style.display = 'none'
         document.getElementById(`palpite`).disabled = false;
+        document.getElementById(`enviar`).disabled = true;
+    }
+    else if(inputPalpite.value != ''){
         document.getElementById(`enviar`).disabled = false;
+        document.getElementById(`palpite`).disabled = false;
     }
 }
 
@@ -58,6 +65,18 @@ function exibeNumeroInicial(){
         exibeNumero(`${erro}`)
         exibeInfoPalpite()
     }
+}
+
+//Função para permitir apenas números inteiros e positivos no input
+function somenteNumeros(num) {
+    var er = /[^0-9]/;
+    er.lastIndex = 0;
+    var campo = num;
+    if (er.test(campo.value)) {
+      campo.value = "";
+    }
+
+    funcionalidadesBotoes()
 }
 
 function exibeInfoPalpite(){
